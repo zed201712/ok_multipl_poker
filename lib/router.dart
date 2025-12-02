15eac2d3361bb2d7b7ca02215c48515768b9c7e0
okawa <a@a.a>
2025-12-03 02:06:13 +0900

    feat: test RoomDemoScreen

--- lib/router.dart
+++ lib/router.dart
@@ -13,6 +13,7 @@ import 'settings/settings_screen.dart';
 import 'style/my_transition.dart';
 import 'style/palette.dart';
 import 'win_game/win_game_screen.dart';
+import 'package:ok_multipl_poker/demo/demo_room_screen.dart';
 
 /// The router describes the game's navigational hierarchy, from the main
 /// screen through settings screens all the way to each individual level.
@@ -20,50 +21,51 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const MainMenuScreen(key: Key('main menu')),
-      routes: [
-        GoRoute(
-          path: 'play',
-          pageBuilder: (context, state) => buildMyTransition<void>(
-            key: const ValueKey('play'),
-            color: context.watch<Palette>().backgroundPlaySession,
-            child: const PlaySessionScreen(key: Key('level selection')),
-          ),
-          routes: [
-            GoRoute(
-              path: 'won',
-              redirect: (context, state) {
-                if (state.extra == null) {
-                  // Trying to navigate to a win screen without any data.
-                  // Possibly by using the browser's back button.
-                  return '/';
-                }
-
-                // Otherwise, do not redirect.
-                return null;
-              },
-              pageBuilder: (context, state) {
-                final map = state.extra! as Map<String, dynamic>;
-                final score = map['score'] as Score;
-
-                return buildMyTransition<void>(
-                  key: const ValueKey('won'),
-                  color: context.watch<Palette>().backgroundPlaySession,
-                  child: WinGameScreen(
-                    score: score,
-                    key: const Key('win game'),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-        GoRoute(
-          path: 'settings',
-          builder: (context, state) =>
-              const SettingsScreen(key: Key('settings')),
-        ),
-      ],
+      builder: (context, state) => const RoomDemoScreen(),
+      //builder: (context, state) => const MainMenuScreen(key: Key('main menu')),
+      // routes: [
+      //   GoRoute(
+      //     path: 'play',
+      //     pageBuilder: (context, state) => buildMyTransition<void>(
+      //       key: const ValueKey('play'),
+      //       color: context.watch<Palette>().backgroundPlaySession,
+      //       child: const PlaySessionScreen(key: Key('level selection')),
+      //     ),
+      //     routes: [
+      //       GoRoute(
+      //         path: 'won',
+      //         redirect: (context, state) {
+      //           if (state.extra == null) {
+      //             // Trying to navigate to a win screen without any data.
+      //             // Possibly by using the browser's back button.
+      //             return '/';
+      //           }
+      //
+      //           // Otherwise, do not redirect.
+      //           return null;
+      //         },
+      //         pageBuilder: (context, state) {
+      //           final map = state.extra! as Map<String, dynamic>;
+      //           final score = map['score'] as Score;
+      //
+      //           return buildMyTransition<void>(
+      //             key: const ValueKey('won'),
+      //             color: context.watch<Palette>().backgroundPlaySession,
+      //             child: WinGameScreen(
+      //               score: score,
+      //               key: const Key('win game'),
+      //             ),
+      //           );
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      //   GoRoute(
+      //     path: 'settings',
+      //     builder: (context, state) =>
+      //         const SettingsScreen(key: Key('settings')),
+      //   ),
+      // ],
     ),
   ],
 );
